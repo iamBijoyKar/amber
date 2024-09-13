c41bc28ac41be83e50e003cb5dba347c092cd248
Bijoy Kar <bijoykar54321@gmail.com>
2024-09-13 23:49:02 +0530

    feat: Add compile method to layers and initialize output length
    At defining the model input_length is now unnecessary, the compile method handles it.

--- amber/layers.py
+++ amber/layers.py
@@ -5,6 +5,7 @@ from .nurons import Neuron
 class InputLayer:
     def __init__(self,nurons:int):
         self.nurons = []
+        self.output_length = nurons
         for _ in range(nurons):
             new_nuron = Neuron() 
             self.nurons.append(new_nuron)
@@ -22,20 +23,26 @@ class InputLayer:
     
 
 class Dense:
-    def __init__(self,nurons:int,input_length:int):
+    def __init__(self,nurons:int):
         self.nurons = []
-        for _ in range(nurons):
-            weights = generate_weights(input_length)
+        self.output_length = nurons
+        self.input_length = None
+
+    def compile(self,input_length):
+        self.input_length = input_length
+        for _ in range(self.output_length):
+            weights = generate_weights(self.input_length)
             bias = generate_bias()
             new_nuron = Neuron(weights,bias) 
             self.nurons.append(new_nuron)
+
 
     def __repr__(self) -> str:
         return f'<amber.Layers.Dense nurons={len(self.nurons)} >'
     
     def forward(self,inputs:list[float]):
-        # if len(inputs) != len(self.nurons):
-        #     raise ValueError(f'Inappropriate input size given at {self.__repr__} layer. Given size is {np.array(inputs).shape} but required is ({len(self.nurons)})')
+        if self.input_length == None:
+            raise RuntimeError(f'The model {self.__repr__} is uncompiled, compile it before run.')
         outputs = []
         for i in range(len(self.nurons)):
             outputs.append(self.nurons[i].forward(inputs))
--- amber/models.py
+++ amber/models.py
@@ -11,6 +11,13 @@ class Model:
                     raise TypeError(f'The first layer should be a <amber.layers.Dense> but recieved {type(layers[i])}')
         self.layers = layers
 
+
+    def compile(self):
+        output_length = self.layers[0].output_length
+        for i in range(1,len(self.layers)):
+            self.layers[i].compile(output_length)
+            output_length = self.layers[i].output_length
+
     def forward(self,inputs:list[float]):
         new_input = inputs
         output = []
