0b1ffe1896412dc698167e1791d88bb3b0723dc2
Bijoy Kar <bijoykar54321@gmail.com>
2024-09-14 00:18:33 +0530

    refactor: Update weight and bias generation to include values at range (-1<=x<=1)

--- amber/utils.py
+++ amber/utils.py
@@ -4,8 +4,8 @@ import numpy as np
 def generate_weights(num:int):
     weights = []
     for _ in range(num):
-        weights.append(random.random())
+        weights.append(random.random()*random.choice([-1,1]))
     return np.array(weights)
 
 def generate_bias():
-    return random.random()
+    return random.random()*random.choice([-1,1])
