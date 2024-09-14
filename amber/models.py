ce8d8cd052457c284f73c95c45a70b91b5bf1c27
Bijoy Kar <bijoykar54321@gmail.com>
2024-09-14 21:21:29 +0530

    feat: loss calculation and fitting to the model class
    Implement forward pass and loss calculation in Model class, default loss calculation function is categorical cross entropy

--- amber/models.py
+++ amber/models.py
@@ -1,4 +1,5 @@
 from .layers import InputLayer, Dense, SoftMax
+from .loss import categorical_cross_entropy
 
 class Model:
     def __init__(self,layers:list):
@@ -10,6 +11,10 @@ class Model:
                 if not isinstance(layers[i],Dense) and not isinstance(layers[i],SoftMax) :
                     raise TypeError(f'The first layer should be a <amber.layers.Dense> but recieved {type(layers[i])}')
         self.layers = layers
+        self.inputs = []
+        self.outputs = []
+        self.true_outputs = []
+        self.loss = None
 
 
     def compile(self):
@@ -19,14 +24,28 @@ class Model:
             output_length = self.layers[i].output_length
 
     def forward(self,inputs:list[float]):
+        self.inputs = inputs # updating the inputs 
         new_input = inputs
         output = []
         for layer in self.layers:
             output = layer.forward(new_input)
             new_input = output
-        
+        self.outputs = new_input # updating the outpush
         return new_input
-            
+    
+    def calc_loss(self,true_outputs:list[float]):
+        if len(true_outputs) != len(self.outputs):
+            raise ValueError(f'Invalid shape recieved! Given ({len(true_outputs)},) , expected ({len(self.outputs)},)')
+        self.true_outputs = true_outputs
+        loss =  categorical_cross_entropy(self.outputs,self.true_outputs)
+        self.loss = loss
+        return loss
+    
+    def fit(self,X:list[float],y:list[float],epoch=1):
+        for _ in range(epoch):
+            self.forward(X)
+            self.calc_loss(y)
+            print(f'Loss : {self.loss}')
 
 
 
