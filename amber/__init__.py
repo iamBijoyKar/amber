627783f283e64c0849e6bb9dcc0ac3ce5dd561b6
Bijoy Kar <bijoykar54321@gmail.com>
2024-09-24 21:08:33 +0530

    update: `__init__.py` added in amber
    I know I am late!!

---
+++ amber/__init__.py
@@ -0,0 +1,13 @@
+#    _____         ___.                 
+#   /  _  \   _____\_ |__   ___________ 
+#  /  /_\  \ /     \| __ \_/ __ \_  __ \
+# /    |    \  Y Y  \ \_\ \  ___/|  | \/
+# \____|__  /__|_|  /___  /\___  >__|   
+#         \/      \/    \/     \/       
+
+# Amber is a custom made Neural Network library written in Python. 
+# I am build this libray to learn more about Neural Networks and 
+# how they work. Maybe in the future I will use this library to 
+# build some cool projects.
+
+
